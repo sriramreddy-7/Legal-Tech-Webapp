ff85eb6559bb09cf02ef248b07c19786c94da4d5
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-12-25 13:44:11 +0530

    text home page

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render ,redirect
 # Create your views here.
 def index(request):
-    return render(request,'index.html')
+    return render(request,'home.html')
 
 # def login(request):
 #     return render(request,'login.html')

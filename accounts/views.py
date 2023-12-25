8e169ed6cdcf9ad6f8449840becaac35dccdb621
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2023-12-25 14:04:24 +0530

    Layout-2

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render ,redirect
 # Create your views here.
 def index(request):
-    return render(request,'home.html')
+    return render(request,'index.html')
 
 # def login(request):
 #     return render(request,'login.html')

65ddb4cf3cafb64d28720a75364042621f809a52
sriramreddy-7 <sriramreddykoonadi@gmail.com>
2024-01-10 12:03:05 +0530

    ADMIN LSP PROFILE

--- legaltech/urls.py
+++ legaltech/urls.py
@@ -57,7 +57,8 @@ urlpatterns = [
     
     # path("login",views.login,name="login"),
 ]
-urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+urlpatterns +=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL,document_root=settings.STATIC_ROOT)
 
 
